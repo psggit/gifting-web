348ebedd9e2286455f96cfc689391f3ac63cc010
Aparna Chinnaiah <aparna.chinnaiah@hipbar.com>
2018-12-22 09:16:50 +0530

    Changed px to % in header style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import Header from "Components/header"
+import Footer from "Components/footer"
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <React.Fragment>
+        <Header />
+        <Footer />
+      </React.Fragment>
     )
   }
 }
