074e7d444923e849ab5c704d15115f5198ea4c24
Madhur Garg <madhur@hipbar.com>
2018-12-22 19:45:36 +0530

    udpated base font sizes

--- src/landing/index.js
+++ src/landing/index.js
@@ -14,19 +14,28 @@ const LandingPage = () => (
           with HipBar Gifting!
           </h2>
 
-          <p className="cm s2">
+          <p className="os s2">
           With HipBar Gift Cards, gift drinks to your friends & family this festive season! Redeem drinks at 50+ retailers across Bengaluru!
           </p>
 
           <div style={{ marginTop: "30px", }}>
-            <Button primary icon="rightArrow">Start Gifting</Button>
+            <Button primary icon="rightArrow">Start Gifting!</Button>
           </div>
+          <p className="os s7">
+            Currently in Bengaluru <br />
+            Coming soon to 10+ cities accros India!
+          </p>
+
         </div>
 
         <div className="col">
           <GiftCard />
         </div>
       </div>
+      <h2 className="cm s2">
+        Get 50%* off on your first HipBar Gift Card!
+      </h2>
+
     </div>
   </div>
 )
