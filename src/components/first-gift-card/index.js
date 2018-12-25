b14024f0c18080372d8bd851015e874bebbc9994
Madhur Garg <madhur@hipbar.com>
2018-12-25 11:30:31 +0530

    first gift card section component

---
+++ src/components/first-gift-card/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Button from "Components/button"
+import "./first-gift-card.scss"
+
+const FirstGiftCard = () => (
+  <div className="first-gift-card">
+    <h1 className="cm s1">
+    Get 50%* off on your first HipBar Gift Card!
+    </h1>
+
+    <p className="os s2">
+    Gift drinks to your friends & family this festive season<br/>
+with HipBar Gift Cards!
+    </p>
+
+    <Button secondary>start gifting!</Button>
+  </div>
+)
+
+export default FirstGiftCard
