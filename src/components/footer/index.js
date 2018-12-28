d7f87ac500a061aac44b89c022dea3853a232020
Aparna Chinnaiah <aparna.chinnaiah@hipbar.com>
2018-12-28 18:05:33 +0530

    Fixed footer issue

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,33 +1,59 @@
 import React from 'react'
-import './footer.scss'
 import Icon from "Components/icon"
+import './footer.scss'
 
-class footer extends React.Component {
-  constructor(props) {
-    super(props)
-    this.items = ["Send Gift Cards", "Using Gift Cards", "Retailer Outlets", "Support"]
-  }
-
-  render() {
-    return(
-      <div className="footer">
-        <span className="logo">
+const Footer = () => (
+  <div>
+    <div className="container footer">
+      <div className="upper">
+        <div className="col">
           <Icon name="hipbarLogo" />
-        </span>
-        <div className="title">HipBar</div>
-        <span className="copyright-text">Copyright 2018 | All Right Reserved.</span>
-        <div className="footer-items">
-          {
-            this.items.map((item, index) => {
-              return <a key={index}>
-                { item }
-              </a>
-            })
-          }
+          <p>HipBar</p>
+          <p>
+            Copyright 2018 | All Right Reserved.
+          </p>
+        </div>
+
+        <div className="col">
+          <div className="footer-item">
+            <ul>
+              <li><a target="_blank" href="/send-gift">Send Gift Cards</a></li>
+              <li><a target="_blank" href="/using-gift-card">Using Gift Cards</a></li>
+              <li><a target="_blank" href="/retail-outlet">Retail Outlets</a></li>
+              <li><a target="_blank" href="/faq">FAQs</a></li>
+            </ul>
+          </div>
+
+          <div className="footer-item">
+            <ul>
+              <li><a target="_blank" href="/privacy">Privacy Policy</a></li>
+              <li><a target="_blank" href="/user-terms">Customer T&amp;C</a></li>
+              <li><a target="_blank" href="/merchants-t-c">Merchant T&amp;C</a></li>
+              <li><a target="_blank" href="/grievance-policy">Grievance Policy</a></li>
+              <li><a target="_blank" href="/gifting-t-c">Gifting T&amp;C</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="footer-item">
+            <p>Hip Bar Private Limited</p>
+            <p>
+              No. 34, (1st Floor) B Ramachandra<br />
+              Adithanar Road, <br />
+              Gandhinagar, Adyar, Chennai, <br />
+              Tamil Nadu 600020
+            </p>
+            {/* <div className="social">
+              <a target="_blank" href='https://facebook.com/hipbarapp'><Icon name="fbLogo" /></a>
+              <a target="_blank" href='https://twitter.com/hipbarapp'><Icon name="twitterLogo" /></a>
+              <a target="_blank" href='https://instagram.com/hipbarapp'><Icon name="instagramLogo" /></a>
+            </div> */}
+          </div>
         </div>
       </div>
-    )
-  }
-}
+    </div>
+  </div>
+)
 
-export default footer
+export default Footer
