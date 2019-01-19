1deabc8978f9061673f5f72e4c05d3b21597d8d0
Aparna Chinnaiah <aparna.chinnaiah@hipbar.com>
2019-01-19 08:27:09 +0530

    UI alignment

--- src/ProductListing/index.js
+++ src/ProductListing/index.js
@@ -5,6 +5,16 @@ import Icon from "Components/icon"
 class ProductListing extends React.Component {
   constructor() {
     super()
+
+    this.state = {
+      search_text: ''
+    }
+
+    this.handleTextChange = this.handleTextChange.bind(this)
+  }
+
+  handleTextChange(e) {
+    this.setState({ [e.target.name]: e.target.value})
   }
 
   render() {
@@ -12,17 +22,39 @@ class ProductListing extends React.Component {
       <div id="ProductListing">
         <div className="content">
           <div className="header">
-            <div>
-              <p className="os s8">Showing products in:</p>
-              <select>
-                <option>Bangalore</option>
-                <option>Chennai</option>
-                <option>Japan</option>
-              </select>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+              <p className="os s8" style={{marginRight: '16px'}}>Showing products in:</p>
+              <div className="form-group" style={{marginTop: '0'}}>
+                <div>
+                  <select>
+                    <option>Bangalore</option>
+                    <option>Chennai</option>
+                    <option>Erode</option>
+                  </select>
+                </div>
+              </div>
             </div>
             <div>
-              <input type="text" placeholder="Search for products" />
-              <span><Icon name="plus" /></span>
+              {/* <input type="text" placeholder="Search for products" />
+              <span><Icon name="plus" /></span> */}
+              <div className="form-group">
+                <div>
+                  <input
+                    type="text"
+                    name="search"
+                    placeholder="Search for products"
+                    //className={`${nameErr.status ? 'error' : ''}`}
+                    value={this.state.search_text}
+                    //disabled={this.state.disableField && this.state.otpSent}
+                    //style={this.state.disableField && this.state.otpSent ? cursorStyle : {}}
+                    autoComplete="off"
+                    onChange={(e) => this.handleTextChange(e)}
+                  />
+                </div>
+                <div >
+                  <span><Icon name="plus" /></span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
