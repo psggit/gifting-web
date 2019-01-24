d5b005566790f81bb20c321b8a2146350d40a8b5
Madhur Garg <madhur@hipbar.com>
2019-01-24 17:20:08 +0530

    genre-item component

---
+++ src/components/GenreItem/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./genre-item.scss"
+
+class GenreItem extends React.Component {
+  render() {
+    return (
+      <div className="genre--item">
+
+      </div>
+    )
+  }
+}
+
+export default GenreItem
