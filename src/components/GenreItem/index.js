0db2a6af37e7f270cc821e44a1a8adb1fe9eff0d
Madhur Garg <madhur@hipbar.com>
2019-01-29 13:33:16 +0530

    added story for genre item

--- src/components/GenreItem/index.js
+++ src/components/GenreItem/index.js
@@ -1,11 +1,33 @@
 import React from "react"
 import "./genre-item.scss"
+import "Sass/base.scss"
+import Icon from "./../icon"
 
 class GenreItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick(e) {
+    this.props.handleClick(parseInt(e.target.id))
+  }
   render() {
     return (
-      <div className="genre--item">
-
+      <div
+        id={this.props.id}
+        onClick={this.handleClick}
+        className={`genre--item ${this.props.active === this.props.id ? "active" : ""}`}>
+        <span><Icon name="drinkChecked" /></span>
+        <div style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          textAlign: "center"
+        }}>
+          <Icon name="drink" />
+          <p className="os" style={{ color: "#777", fontWeight: "bold" }}>{this.props.name}</p>
+        </div>
       </div>
     )
   }
