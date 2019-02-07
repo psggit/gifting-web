97dbabf620eaa5d3d812fce14646cafd7c2674ee
Madhur Garg <madhur@hipbar.com>
2019-02-07 19:27:26 +0530

    bugfixes: modal box alignment

--- src/components/GiftMoreDrinks/index.js
+++ src/components/GiftMoreDrinks/index.js
@@ -3,13 +3,19 @@ import Icon from "Components/icon"
 import Button from "Components/button"
 import "./gift-more-drinks.scss"
 
+function getUrl() {
+  const u = location.pathname.split("/")
+  u.pop()
+  return u.join("/")
+}
+
 function GiftMoreDrinks() {
   return (
     <div className="gift--more-drinks">
-      <div>
+      <a href={getUrl()}>
         <Icon name="giftIcon" />
         <p className="os s5">Gift more drinks</p>
-      </div>
+      </a>
       <Button primary icon="rightArrowWhite">Gift basket</Button>
     </div>
   )
