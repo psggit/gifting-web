d5b3c6bf7e2ef3cd442f182595dba9f2aa5f0f32
Madhur Garg <madhur@hipbar.com>
2019-02-14 17:45:17 +0530

    landing page images

--- src/components/icon/icons.js
+++ src/components/icon/icons.js
@@ -31,7 +31,7 @@ export default {
     </svg>
   ),
   hipbarLogoWhite: (
-    <svg width="60px" height="60px" viewBox="0 0 74 78" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+    <svg width="100px" height="38.57px" viewBox="0 0 74 78" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
       <g id="V0.3---With-Mobile-Version" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
         <g id="01a-Gifitng-Website---V0.7---Home-Page" transform="translate(-858.000000, -336.000000)">
           <g id="Group-2" transform="translate(80.000000, 214.000000)">
@@ -1083,6 +1083,69 @@ export default {
                 </g>
             </g>
         </g>
+    </svg>
+  ),
+  'leftArrow': (
+    <svg width="9px" height="14px" viewBox="0 0 9 14" version="1.1">
+        <g id="Account-Settings" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="Audit-Log" transform="translate(-1248.000000, -386.000000)" fill="#5A6872">
+                <g id="Group-2" transform="translate(273.000000, 310.000000)">
+                    <g id="Table" transform="translate(0.000000, 70.000000)">
+                        <g id="Table-Controls" transform="translate(37.000000, 0.000000)">
+                            <g id="Pagination" transform="translate(845.919864, 0.000000)">
+                                <g id="Icon/UI/Chevron/Light-UI" transform="translate(96.484228, 13.000000) scale(-1, 1) translate(-96.484228, -13.000000) translate(91.996590, 7.000000)">
+                                    <g id="Chevron-/-Light-UI" transform="translate(4.500000, 6.000000) scale(-1, 1) rotate(90.000000) translate(-4.500000, -6.000000) translate(-2.250000, 2.000000)">
+                                        <polygon id="Shape" points="11.925 -4.4408921e-16 6.75 4.66216216 1.575 -4.4408921e-16 0 1.41891892 6.75 7.5 13.5 1.41891892"></polygon>
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </svg>
+  ),
+  'rightArrow': (
+    <svg width="8px" height="14px" viewBox="0 0 8 14" version="1.1">
+        <g id="Account-Settings" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="Audit-Log" transform="translate(-1350.000000, -386.000000)" fill="#5A6872">
+                <g id="Group-2" transform="translate(273.000000, 310.000000)">
+                    <g id="Table" transform="translate(0.000000, 70.000000)">
+                        <g id="Table-Controls" transform="translate(37.000000, 0.000000)">
+                            <g id="Pagination" transform="translate(845.919864, 0.000000)">
+                                <g id="Icon/UI/Chevron/Light-UI" transform="translate(194.090367, 7.000000)">
+                                    <g id="Chevron-/-Light-UI" transform="translate(4.500000, 6.000000) scale(-1, 1) rotate(90.000000) translate(-4.500000, -6.000000) translate(-2.250000, 2.000000)">
+                                        <polygon id="Shape" points="11.925 -4.4408921e-16 6.75 4.66216216 1.575 -4.4408921e-16 0 1.41891892 6.75 7.5 13.5 1.41891892"></polygon>
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </svg>
+  ),
+  'location': (
+    <svg version="1.1" id="Capa_1" x="0px" y="0px" width='30px' height='30px'
+    viewBox="0 0 512 512" style={{enableBackground:'new 0 0 512 512'}}>
+		<g>
+			<g>
+				<path d="M256,0C156.748,0,76,80.748,76,180c0,33.534,9.289,66.26,26.869,94.652l142.885,230.257
+						c2.737,4.411,7.559,7.091,12.745,7.091c0.04,0,0.079,0,0.119,0c5.231-0.041,10.063-2.804,12.75-7.292L410.611,272.22
+						C427.221,244.428,436,212.539,436,180C436,80.748,355.252,0,256,0z M384.866,256.818L258.272,468.186l-129.905-209.34
+						C113.734,235.214,105.8,207.95,105.8,180c0-82.71,67.49-150.2,150.2-150.2S406.1,97.29,406.1,180
+						C406.1,207.121,398.689,233.688,384.866,256.818z"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<path d="M256,90c-49.626,0-90,40.374-90,90c0,49.309,39.717,90,90,90c50.903,0,90-41.233,90-90C346,130.374,305.626,90,256,90z
+								M256,240.2c-33.257,0-60.2-27.033-60.2-60.2c0-33.084,27.116-60.2,60.2-60.2s60.1,27.116,60.1,60.2
+						C316.1,212.683,289.784,240.2,256,240.2z"/>
+			</g>
+		</g>
     </svg>
   )
 }
