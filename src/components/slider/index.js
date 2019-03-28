a6a4663f2e2bcae11a33053b47f2b0e6ba000be6
Aparna Chinnaiah <aparna.chinnaiah@hipbar.com>
2019-03-28 16:14:26 +0530

    Debug - initial slide to load

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -27,6 +27,7 @@ const settings = {
   speed: 500,
   slidesToShow: 3,
   slidesToScroll: 3,
+  initialSlide: 0,
   // nextArrow: <SampleNextArrow />,
   // prevArrow: <SamplePrevArrow />,
   responsive: [
