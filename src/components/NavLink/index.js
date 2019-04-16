d1898f66c742e4f5e8eddf8753b0ae62995745b9
Madhur Garg <madhur@hipbar.com>
2019-04-16 17:33:14 +0530

    added navlink component

---
+++ src/components/NavLink/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function NavLink({ href, children, history, extendedOnclick }) {
+  return (
+    <a
+      onClick={(e) => {
+        e.preventDefault()
+        const path = "/" + e.currentTarget.href.split("/").slice(3).join("/")
+        history.push(path)
+        extendedOnclick()
+      }}
+      href={href}>{children}
+    </a>
+  )
+}
+
+export default NavLink
