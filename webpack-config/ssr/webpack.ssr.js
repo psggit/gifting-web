e4c8d250e9e30f0bc3a1ae572ae8422e817678e8
aparna <aparna.chinnaiah@hipbar.com>
2019-09-19 15:34:30 +0530

    Debug

--- webpack-config/ssr/webpack.ssr.js
+++ webpack-config/ssr/webpack.ssr.js
@@ -1,7 +1,7 @@
 const path = require("path")
 const webpack = require("webpack")
 const nodeExternals = require("webpack-node-externals")
-const CleanWebpackPlugin = require("clean-webpack-plugin")
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
 
 const config = {
   mode: "production",
