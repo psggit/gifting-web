cdc139abddf84e78f412dd2985977fc7ccb0234e
aparna <aparna.chinnaiah@hipbar.com>
2019-12-20 14:39:09 +0530

    Added log in server for printing gtm container id'

--- server/index.js
+++ server/index.js
@@ -53,6 +53,9 @@ app.use(helmet({
 // const PROD_API_BASE = process.env.PROD_API_BASE
 // const BASE_URL = "amebae21.hasura-app.io";
 const BASE_URL = process.env.BASE_URL || "hipbar-dev.com"
+const GTM_CONTAINER_ID = process.env.GTM_CONTAINER_ID
+
+console.log("gtm container id", GTM_CONTAINER_ID)
 
 app.get("/images/:name", (req, res) => {
   res.sendFile(path.join(__dirname, `./../images/${req.params.name}`), (err) => {
